2329fc31ff9ed18af4ef79c0cf067a3b81ddfe7b
KojoBoat <egyirchristian07@gmail.com>
2019-07-17 16:04:14 +0000

    Imported the url_for  library

--- myapp.py
+++ myapp.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ app = Flask(__name__)
 @app.route('/foo/<name>')
 def foo(name):
 	return render_template('index.html',to=name)
+
+
+#bootstrap forms
+@app.route('/forms')
+def forms():
+	return render_template('/forms.html')
+
 
 @app.route('/whereami')
 def whereami():
