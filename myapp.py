a933c1bf98f4ee708f3a6dec7ed68523607f187d
Christian <egyirchristian07@gmail.com>
2019-07-17 14:06:46 +0000

    Made the web dynamic

--- myapp.py
+++ myapp.py
@@ -2,9 +2,13 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template('index.html')
+#@app.route('/')
+#def index():
+#	return render_template('index.html')
+
+@app.route('/foo/<name>')
+def foo(name):
+	return render_template('index.html',to=name)
 
 @app.route('/whereami')
 def whereami():
