14bbb312ffa3504ee897baeaf9d7a28b19a8e1c6
Christian <egyirchristian07@gmail.com>
2019-07-17 11:11:13 +0000

    Built a homepage

--- myapp.py
+++ myapp.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return 'Hello Christian'
+	return render_template('index.html')
 
 @app.route('/whereami')
 def whereami():
@@ -12,10 +12,3 @@ def whereami():
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
-
-
-
-
-
-
-
