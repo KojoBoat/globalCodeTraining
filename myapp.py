df681c6ff905fd5e52db725f68d7e6adf948897c
Christian <egyirchristian07@gmail.com>
2019-07-17 10:55:29 +0000

    Added another route Flask

--- myapp.py
+++ myapp.py
@@ -6,7 +6,16 @@ app = Flask(__name__)
 def index():
 	return 'Hello Christian'
 
+@app.route('/whereami')
+def whereami():
+	return 'I am in Ghana,a participant of this years Global Code!!'
+
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
 
 
